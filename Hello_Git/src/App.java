2e06aa0097ecf667a4123eba53ee705155358582
sibu <sibu@192.168.8.100>
2019-03-01 07:20:41 +0200

    Second Commit

--- Hello_Git/src/App.java
+++ Hello_Git/src/App.java
@@ -2,8 +2,8 @@
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World");
-
+		//System.out.println("Hellow World");
+		System.out.println("Made changes to the Git Example");
 	}
 
 }
